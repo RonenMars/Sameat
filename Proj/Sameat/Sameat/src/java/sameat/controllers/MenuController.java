59c8ba1eb9dfb5095ad7e56cdc54f8ddc9e731c3
nextar26 <galili409@gmail.com>
2015-05-12 12:03:29 +0300

    תחילת עבודה

---
+++ Proj/Sameat/Sameat/src/java/sameat/controllers/MenuController.java
@@ -0,0 +1,65 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import sameat.data.UsersDB;
+
+import sameat.util.CheckData;
+import sameat.business.User;
+
+/**
+ *
+ * @author plotk_000
+ */
+@WebServlet(name = "MenuController", urlPatterns = {"/MenuController"})
+public class MenuController extends HttpServlet {
+    
+    CheckData cd=new CheckData();
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       doPost(request , response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+     
+       
+
+
+       
+    }
+}
